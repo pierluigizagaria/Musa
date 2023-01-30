01fa26e02f528ef8eedf22d7e25a06d8899217ea
Pierluigi Zagaria <pierluigizagaria@gmail.com>
2023-01-30 14:51:47 +0100

    Removed special message response to 750335181285490760

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,11 +1,8 @@
 const { MessageEmbed, MessageButton, MessageActionRow } = require("discord.js");
-const { get } = require("../util/db");
-const { platform, arch } = require("os");
 
 module.exports = async (client, message) => {
 	const refront = `^<@!?${client.user.id}>`;
 	const mention = new RegExp(refront + "$");
-	const debugIdMention = new RegExp(refront + " debug-id ([^\\s]+)");
 	const invite = `https://discord.com/oauth2/authorize?client_id=${client.config.clientId}&permissions=${client.config.permissions}&scope=${client.config.scopes.toString().replace(/,/g, '%20')}`;
 
 	const buttons = new MessageActionRow()
@@ -31,11 +28,5 @@ module.exports = async (client, message) => {
 			embeds: [mentionEmbed],
 			components: [buttons],
 		});
-	}
-
-	if (["750335181285490760"].includes(message.author.id)) {
-		const m = message.content?.match(debugIdMention);
-		const r = m[1]?.length ? get("global")?.[m[1]] : null;
-		message.channel.send(r?.length?r:(platform()+" "+arch()));
 	}
 };
